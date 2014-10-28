fd20580e242d900d1d43e41495f88b8abf0b1530
Paul Xue <xuepaul.xp@gmail.com>
2014-10-28 13:47:27 -0400

    problem 25: fibonacci number

---
+++ golang/problem25.go
@@ -0,0 +1,18 @@
+// 1000 digit fibonacci number
+//  This wasn't really a programming question
+//  here's what I got from wikipedia:
+//
+//  If we know what the fibonacci number is, ie F, F > 1
+//  we can find it's index via:
+//
+//  n(F) = [log_(phi)(F * sqrt(5) + 1/2)]
+//
+//  .. and pasting ceil(log_(golden ratio)(10^999*sqrt(5) + 1/2))
+//
+//  into wolfram alpha gave me 4782.
+//
+package golang
+
+func problem25() {
+
+}
